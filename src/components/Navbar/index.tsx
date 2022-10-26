3aca67338edc3997693e3195ea1265ae7d6fd404
anshulraheja <anshul.r1610@gmail.com>
2022-10-27 02:35:10 +0530

    add login button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
-import { DownOutlined, ShoppingCartOutlined } from '@ant-design/icons';
-import { Avatar, Badge, Col, Dropdown, Input, Menu, Row, Space } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Avatar, Badge, Button, Col, Dropdown, Input, Menu, Row, Space } from 'antd';
 import './navbar.scss';
 const { Search } = Input;
 const Navbar = () => {
@@ -39,7 +39,7 @@ const Navbar = () => {
 				<Col md={6} className="search-bar" style={{ padding: '0px' }}>
 					<Search placeholder="Search for products, brands and more" onSearch={onSearch} enterButton />
 				</Col>
-				<Col md={1} offset={8}>
+				{/* <Col md={1} offset={8}>
 					<Dropdown overlay={menu}>
 						<a onClick={(e) => e.preventDefault()}>
 							<Space className="nav-cta" size={4}>
@@ -47,6 +47,13 @@ const Navbar = () => {
 								<DownOutlined className="nav-down-icon" />
 							</Space>
 						</a>
+					</Dropdown>
+				</Col> */}
+				<Col md={1} offset={8}>
+					<Dropdown overlay={menu} arrow>
+						<Button className="btn-login">
+							<a href="/login">Login</a>
+						</Button>
 					</Dropdown>
 				</Col>
 				<Col md={1}>
